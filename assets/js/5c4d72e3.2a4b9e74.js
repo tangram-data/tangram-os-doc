c0051f1352446ecbebcb82b8790ec99ca31a819e
Jian TANG <jtang1126@gmail.com>
2025-04-16 17:59:01 +0100

    Deploy website - based on 7ed1b67beac17daa651ee4712b21764be7325014

--- assets/js/5c4d72e3.2a4b9e74.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4452],{7601:e=>{e.exports=JSON.parse('{"categoryGeneratedIndex":{"title":"Get Started","slug":"/category/get-started","permalink":"/tangram-os-doc/category/get-started","sidebar":"tutorialSidebar","navigation":{"next":{"title":"What is Tangram OS","permalink":"/tangram-os-doc/"}}}}')}}]);
